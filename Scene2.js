e607c0924c8778932980c07ecf070e1aeb606267
Unknwn <96542420+AMBprog@users.noreply.github.com>
2024-04-03 19:02:05 +0200

    Retrait du background
    Jump to scene 2 ok

--- Scene2.js
+++ Scene2.js
@@ -3,15 +3,9 @@ class Scene2 extends Phaser.Scene {
         super("playGame");
     }
 
-    create() {
-        this.background = this.add.image(0,0,"background");
-        this.background.setOrigin(0,0);
-    }
+
 
     create (){
-        this.add.text(20, 20, "Plyaing game", {
-            font: "25px Arial", 
-            fill: Yellow
-        });
+        this.add.text(20, 20, "Playing game", {font: "25px Arial", fill: "yellow"});
     }
-}    
+}
